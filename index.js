78360002adeb08aabfb08666bf89137b11b4655f
Adam Ziegele <adamziegele@Adams-MacBook-Pro.local>
2017-09-16 11:54:22 -0400

    New files

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+//JQUERY
+
+var element = $("#elementId");
+
+var element = $(".className");
+
+element2.append(element1) // append is for JQ - Adds element 1inside El2 as its last child
+
+element1.html()
+
+element1.css("color:#4433AA")
